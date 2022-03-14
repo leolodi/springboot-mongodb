4f2210393cd0659be7c0084477e7ec0f51bfd0c0
Leonardo Lodi <leonardo.tlodi@gmail.com>
2022-03-14 20:28:16 +1100

    Projection of author data with DTO

---
+++ src/main/java/com/ltl/mongodb/dto/AuthorDTO.java
@@ -0,0 +1,36 @@
+package com.ltl.mongodb.dto;
+
+import java.io.Serializable;
+
+import com.ltl.mongodb.domain.User;
+
+public class AuthorDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+	}
+	
+	public AuthorDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
